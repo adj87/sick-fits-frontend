94223af3b81a4360ac64601194ee574b8ae82ab4
Dr Jauregui <albertodomjauregui@hotmail.com>
2021-04-21 18:27:23 +0200

    create some components

---
+++ pages/orders.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function OrderPage() {
+    return (
+        <div>
+            OrderPage
+        </div>
+    )
+}
---
+++ pages/sell.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function SellPage() {
+    return (
+        <div>
+            SellPage
+        </div>
+    )
+}
