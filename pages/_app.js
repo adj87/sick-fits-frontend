f7ed531c205d720b1ff0869238a8313744c723e2
Dr Jauregui <albertodomjauregui@hotmail.com>
2021-04-23 08:39:12 +0200

    implement loader in pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import Page from "../components/Page";
+import { Router } from "next/dist/client/router";
+import nProgress from "nprogress";
+
+import "nprogress/nprogress.css";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => nProgress.start());
+Router.events.on("routeChangeComplete", () => nProgress.done());
+Router.events.on("routeChangeError", () => nProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
