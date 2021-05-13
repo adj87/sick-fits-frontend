555e18dafac9cbe11dad6f4f904fed29007ff436
Dr Jauregui <albertodomjauregui@hotmail.com>
2021-05-13 19:12:33 +0200

    pagination

---
+++ components/Pagination.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import PaginationStyles from './styles/PaginationStyles';
+import Link from 'next/link';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import { perPage } from '../config';
+
+const PAGINATION_QUERY = gql`
+  query {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+  const count = data?._allProductsMeta?.count;
+  const pageCount = Math.ceil(count / perPage);
+  if (loading) return 'loading...';
+  if (error) return 'error';
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Sick - fits</title>
+      </Head>
+      <Link href={`/products/${page - 1}`} aria-disabled={page === 1}>
+        <a aria-disabled={page <= 1}>Prev </a>
+      </Link>
+      <p>Page 1 of {pageCount}</p>
+      <p>Items total {count}</p>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Prev </a>
+      </Link>
+    </PaginationStyles>
+  );
+}
