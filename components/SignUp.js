815d78121b188f08122708520966233e0e07066c
alberto <alberto.jauregui@fujitsu.com>
2021-05-28 20:41:28 +0200

    create signup

--- components/SignUp.js
+++ components/SignUp.js
@@ -5,58 +5,74 @@ import { useMutation } from '@apollo/client';
 import { CURRENT_USER_QUERY } from './User';
 import ErrorMessage from '../components/ErrorMessage';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNIN_MUTATION(
+    $email: String!
+    $password: String!
+    $name: String!
+  ) {
+    createUser(data: { email: $email, password: $password, name: $name }) {
+      id
+      email
+      name
     }
   }
 `;
 
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
     password: '',
+    name: '',
   });
 
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
     variables: inputs,
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    //  refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await signin();
+    const res = await signup().catch(console.error);
     console.log(error);
   };
 
-  const error =
-    data?.authenticateUserWithPassword.__typename ==
+  /*   const error =
+    data?.authenticateUserWithPassword?.__typename ==
     'UserAuthenticationWithPasswordFailure'
       ? data?.authenticateUserWithPassword
-      : undefined;
+      : undefined; */
 
   return (
     <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign into account</h2>
+      <h2>Sign Up for an account</h2>
       <ErrorMessage error={error} />
       <fieldset>
+        {data?.createUser && (
+          <>
+            <p>
+              Signed up with {data.createUser.email} - Please Go ahead and sign
+              in
+            </p>
+          </>
+        )}
         <label htmlFor="email">
           Email
           <input
             type="text"
             name="email"
+            autoComplete="email"
             value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Name
+          <input
+            type="text"
+            name="name"
+            autoComplete="name"
+            value={inputs.name}
             onChange={handleChange}
           />
         </label>
@@ -65,6 +81,7 @@ export default function SignIn() {
           <input
             type="password"
             name="password"
+            autoComplete="password"
             value={inputs.password}
             onChange={handleChange}
             placeholder="Your password here"
